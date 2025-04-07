dde722a141187d9ad2b85f9579a6b41c3d8d1629
subhra8888 <123.shubhro@gmail.com>
2025-04-07 12:19:06 +0530

    Update 01. Top 5 Cities Spending Contribution.sql

--- 01. Top 5 Cities Spending Contribution.sql
+++ 01. Top 5 Cities Spending Contribution.sql
@@ -1,9 +1,11 @@
 --write a query to print top 5 cities with highest spends and their percentage contribution of total credit card spends
 
-with total_spends_cte as(
-select sum(amount) as total_spends from credit_card_transactions)
-select top 5 city, sum(amount) as city_wise_spends, total_spends,
-cast(sum(amount)*1.0/total_spends*100 as decimal (5,2)) as percentage_contribution
-from credit_card_transactions, total_spends_cte
-group by city, total_spends
-order by percentage_contribution desc;
+with cte as(
+select sum(amount) as total_sales from credit_card_transactions),
+cte2 as(
+select city, sum(amount) as city_wise_sales
+from credit_card_transactions
+group by city)
+select top 5 *, cast(city_wise_sales*1.0/ total_sales * 100 as decimal (10,2)) as percentage
+from cte2, cte
+order by city_wise_sales desc;
