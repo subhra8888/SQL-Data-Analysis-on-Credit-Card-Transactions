6d20099eca4ea13d3432a59adcb16b86c4c33284
Subhra Banerjee <123.shubhro@gmail.com>
2025-06-10 16:52:53 +0530

    Delete 06. Female Spend Percentage by Expense type.sql

--- 06. Female Spend Percentage by Expense type.sql
+++
@@ -1,9 +0,0 @@
---write a query to find percentage contribution of spends by females for each expense type
-
-select exp_type,
-sum(case when gender = 'F' then amount else 0 end) as female_spends,
-sum(amount) as total_spends,
-cast(sum(case when gender = 'F' then amount else 0 end)*1.0/sum(amount)*100 as decimal (5,2)) as percentage_contribution
-from credit_card_transactions
-group by exp_type
-order by percentage_contribution desc;
